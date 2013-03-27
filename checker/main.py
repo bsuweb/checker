446f96a5a3a9d1dbf84783f4f6f8998078352441
Matt Buresh <mattburesh@gmail.com>
2013-03-27 10:20:55 -0500

    Initial update. main.py goes to the given directory and runs all python scripts inside.

---
+++ checker/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import getopt
+
+
+class Chdir:
+    def __init__(self, newPath):
+        self.savedPath = os.getcwd()
+        os.chdir(newPath)
+
+
+class Checker:
+    def __init__(self, path):
+        self.path = path
+
+    def get_jobs(self):
+        Chdir(self.path)
+        jobs = []
+        for dirname, dirnames, filenames in os.walk('.'):
+            for filename in filenames:
+                i = os.path.join(dirname, filename)
+                if i.lower().endswith('.py') and i != "./__init__.py":
+                    jobs.append(self.path + i[2:])
+        self.run_jobs(jobs)
+
+    def run_jobs(self, jobs):
+        for job in jobs:
+            subprocess.check_call(["python", job])
+
+
+if __name__ == '__main__':
+    opts, path = getopt.getopt(sys.argv[1], "h")
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'help information'
+            sys.exit()
+    check = Checker(path)
+    check.get_jobs()
