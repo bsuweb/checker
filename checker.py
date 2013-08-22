ab1bdc7612168fbeb839f5243321da23e0900d34
Matt Buresh <mattburesh@gmail.com>
2013-08-22 14:44:50 -0500

    Reformatting.

--- checker.py
+++ checker.py
@@ -26,6 +26,36 @@ class Checker:
                     files.append(filename_path)
         return files
 
+    def fail(self, job, current, ret, now, db, out):
+        # value is information to be saved in the database.
+        # message is the information to be sent in the email.
+        value = out[0]
+        message = out[1]
+        # Get the number of fails for the current job, and increment
+        try:
+            cur_fails = db.query("SELECT fails FROM "+current)[-1][0] + 1
+        except IndexError:
+            cur_fails = 1
+        # Add new entry to the current job table containing the time,
+        # exit code, value, and current number of fails. 
+        db.insert_values(current, "('"+str(now)+"',"+str(ret)+",'"+value+"',"+str(cur_fails)+")")
+        # Check to see if anyone needs to be notified of the failure.
+        users = db.query("SELECT email, checks FROM users")
+        for user in users:
+            email = user[0]
+            jobs = user[1]
+            for x in glob.glob(jobs):
+                if job in abspath(join(getcwd(), x)):
+                    max_fails = db.query("SELECT fails FROM users WHERE email ="+"'"+email+"'")[0][0]
+                    cur_notify = db.query("SELECT cur_notify FROM users WHERE email ="+"'"+email+"'")[0][0]
+                    max_notify = db.query("SELECT notify FROM users WHERE email ="+"'"+email+"'")[0][0]
+                    if cur_fails >= max_fails and cur_notify <= max_notify:
+                        # Send Message
+                        # mail = contact.Email(email, message)
+                        # mail.send()
+                        # Update number of notifications user has been sent
+                        db.update('UPDATE users SET cur_notify = cur_notify + 1 WHERE email ='+'"'+email+'"')
+
     def run(self):
         for job in self.jobs:
             current = basename(splitext(job)[0])
@@ -41,46 +71,15 @@ class Checker:
             db.make(current, ' (datetime TEXT, exit_code INTEGER, value TEXT, fails INTEGER)')
             out = out[0].splitlines()
             if ret > 0:
+                print "Fail - " + job
                 # Something went wrong with the current job.
-                # value is information to be saved in the database.
-                # message is the information to be sent in the email.
-                value = out[0]
-                message = out[1]
-                # Get the number of fails for the current job, and increment
-                try:
-                    cur_fails = db.query("SELECT fails FROM "+current)[-1][0] + 1
-                    print cur_fails
-                except IndexError:
-                    cur_fails = 1
-                    print "INDEX ERROR : " + str(cur_fails)
-                # Add new entry to the current job table containing the time,
-                # exit code, value, and current number of fails. 
-                db.insert_values(current, "('"+str(now)+"',"+str(ret)+",'"+value+"',"+str(cur_fails)+")")
-                # Check to see if anyone needs to be notified of the failure.
-                users = db.query("SELECT email, checks FROM users")
-                for user in users:
-                    email = user[0]
-                    jobs = user[1]
-                    for x in glob.glob(jobs):
-                        if job in abspath(join(getcwd(), x)):
-                            max_fails = db.query("SELECT fails FROM users WHERE email ="+"'"+email+"'")[0][0]
-                            cur_notify = db.query("SELECT cur_notify FROM users WHERE email ="+"'"+email+"'")[0][0]
-                            max_notify = db.query("SELECT notify FROM users WHERE email ="+"'"+email+"'")[0][0]
-                            print "MAXFAILS: "+str(max_fails)
-                            print "CURNOT: "+str(cur_notify)
-                            print "MAXNOT: "+str(max_notify)
-                            if cur_fails >= max_fails and cur_notify <= max_notify:
-                                # Send Message
-                                # mail = contact.Email(email, message)
-                                # mail.send()
-                                print "Current Fails: " + str(cur_fails) + " Max Fails: " + str(max_fails)
-                                # Update number of notifications user has been sent
-                                db.update('UPDATE users SET cur_notify = cur_notify + 1 WHERE email ='+'"'+email+'"')
-                                print "Current Notifications: " + str(cur_notify) + " Max Notifications: " + str(max_notify)
-            # Job ran successfully.
+                Checker.fail(self, job, current, ret, now, db, out)
             else:
-                print "No Errors"
+                # Job ran successfully.
+                print "No Fails"
                 db.insert_values(current, "('"+str(now)+"',"+str(ret)+",'"+out[0]+"',"+str(0)+")")
+                # Reset value for cur_notify in the users table
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
